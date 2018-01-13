dab12e7fa9267eac2e2a6a99450315726b1f882d
SiIky <siiky@users.noreply.github.com>
2018-01-13 19:59:29 +0000

    Add <common.h> header file with `max()` and `min()` macros

---
+++ include/common.h
@@ -0,0 +1,12 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#ifndef max
+# define max(a, b) (((a) > (b)) ? (a) : (b))
+#endif /* max */
+
+#ifndef min
+# define min(a, b) (((a) < (b)) ? (a) : (b))
+#endif /* min */
+
+#endif /* _COMMON_H */
