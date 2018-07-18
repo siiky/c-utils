80b6b06abc2147c33545ad512e69c6e4f3ede137
SiIky <siiky@users.noreply.github.com>
2018-07-18 16:35:34 +0100

    tests: Add VEC_FOREACH_RANGE()
    Test side effects with both VEC_FOREACH_RANGE() & VEC_FOREACH()

--- tests/vec/foreach.c
+++ tests/vec/foreach.c
@@ -6,9 +6,12 @@
 
 #include "vec.h"
 
+static size_t what = 0;
+
 static void qc_vec_foreach_do_nothing_func (const int x)
 {
     UNUSED(x);
+    what++;
 }
 
 static enum theft_trial_res qc_vec_foreach_len_prop (struct theft * t, void * arg1)
@@ -18,6 +21,7 @@ static enum theft_trial_res qc_vec_foreach_len_prop (struct theft * t, void * ar
 
     size_t pre_len = vec->length;
     vec_foreach(vec, qc_vec_foreach_do_nothing_func);
+    what = 0;
     size_t pos_len = vec->length;
 
     return QC_BOOL2TRIAL(pre_len == pos_len);
@@ -34,6 +38,7 @@ static enum theft_trial_res qc_vec_foreach_iter_prop (struct theft * t, void * a
     unsigned char pre_reverse = vec->reverse;
 
     vec_foreach(vec, qc_vec_foreach_do_nothing_func);
+    what = 0;
 
     size_t pos_idx = vec->idx;
     unsigned char pos_iterating = vec->iterating;
@@ -59,11 +64,25 @@ static enum theft_trial_res qc_vec_foreach_content_prop (struct theft * t, void 
 
     size_t pre_len = vec->length;
     vec_foreach(vec, qc_vec_foreach_do_nothing_func);
+    what = 0;
 
     bool res = pre_len == 0
         || memcmp(dup->ptr, vec->ptr, pre_len * sizeof(int)) == 0;
 
     qc_vec_dup_free(dup);
+
+    return QC_BOOL2TRIAL(res);
+}
+
+static enum theft_trial_res qc_vec_foreach_side_effects_prop (struct theft * t, void * arg1)
+{
+    UNUSED(t);
+    struct vec * vec = arg1;
+
+    size_t pre_len = vec->length;
+    vec_foreach(vec, qc_vec_foreach_do_nothing_func);
+    bool res = what == pre_len;
+    what = 0;
 
     return QC_BOOL2TRIAL(res);
 }
@@ -77,9 +96,11 @@ static enum theft_trial_res qc_vec_foreach_content_prop (struct theft * t, void 
 QC_MKTEST_FOREACH(content);
 QC_MKTEST_FOREACH(iter);
 QC_MKTEST_FOREACH(len);
+QC_MKTEST_FOREACH(side_effects);
 
 QC_MKTEST_ALL(qc_vec_foreach_test_all,
         qc_vec_foreach_content_test,
         qc_vec_foreach_iter_test,
         qc_vec_foreach_len_test,
+        qc_vec_foreach_side_effects_test,
         );
---
+++ tests/vec/foreach_range.c
@@ -0,0 +1,116 @@
+#include <stdbool.h>
+#include <string.h>
+
+#include <common.h>
+#include <unused.h>
+
+#include "vec.h"
+
+static size_t what = 0;
+
+static void qc_vec_foreach_range_do_nothing_func (const int x)
+{
+    UNUSED(x);
+    what++;
+}
+
+static enum theft_trial_res qc_vec_foreach_range_len_prop (struct theft * t, void * arg1, void * arg2, void * arg3)
+{
+    UNUSED(t);
+    struct vec * vec = arg1;
+    size_t from = * (size_t *) arg2;
+    size_t to = * (size_t *) arg3;
+
+    size_t pre_len = vec->length;
+    vec_foreach_range(vec, qc_vec_foreach_range_do_nothing_func, from, to);
+    what = 0;
+    size_t pos_len = vec->length;
+
+    return QC_BOOL2TRIAL(pre_len == pos_len);
+}
+
+static enum theft_trial_res qc_vec_foreach_range_iter_prop (struct theft * t, void * arg1, void * arg2, void * arg3)
+{
+    UNUSED(t);
+
+    struct vec * vec = arg1;
+    size_t from = * (size_t *) arg2;
+    size_t to = * (size_t *) arg3;
+
+    size_t pre_idx = vec->idx;
+    unsigned char pre_iterating = vec->iterating;
+    unsigned char pre_reverse = vec->reverse;
+
+    vec_foreach_range(vec, qc_vec_foreach_range_do_nothing_func, from, to);
+    what = 0;
+
+    size_t pos_idx = vec->idx;
+    unsigned char pos_iterating = vec->iterating;
+    unsigned char pos_reverse = vec->reverse;
+
+    bool ret = true
+        && pre_iterating == pos_iterating
+        && pre_reverse   == pos_reverse
+        && pre_idx       == pos_idx;
+
+    return QC_BOOL2TRIAL(ret);
+}
+
+static enum theft_trial_res qc_vec_foreach_range_content_prop (struct theft * t, void * arg1, void * arg2, void * arg3)
+{
+    UNUSED(t);
+
+    struct vec * vec = arg1;
+    size_t from = * (size_t *) arg2;
+    size_t to = * (size_t *) arg3;
+
+    struct vec * dup = qc_vec_dup_contents(vec);
+    if (dup == NULL)
+        return THEFT_TRIAL_SKIP;
+
+    size_t pre_len = vec->length;
+    vec_foreach_range(vec, qc_vec_foreach_range_do_nothing_func, from, to);
+    what = 0;
+
+    bool res = pre_len == 0
+        || memcmp(dup->ptr, vec->ptr, pre_len * sizeof(int)) == 0;
+
+    qc_vec_dup_free(dup);
+
+    return QC_BOOL2TRIAL(res);
+}
+
+static enum theft_trial_res qc_vec_foreach_range_side_effects_prop (struct theft * t, void * arg1, void * arg2, void * arg3)
+{
+    UNUSED(t);
+    struct vec * vec = arg1;
+    size_t from = * (size_t *) arg2;
+    size_t to = * (size_t *) arg3;
+
+    size_t pre_len = vec->length;
+    vec_foreach_range(vec, qc_vec_foreach_range_do_nothing_func, from, to);
+    bool res = what == pre_len;
+    what = 0;
+
+    return QC_BOOL2TRIAL(res);
+}
+
+#define QC_MKTEST_FOREACH_RANGE(TEST)                 \
+    QC_MKTEST(qc_vec_foreach_range_ ## TEST ## _test, \
+            prop3,                                    \
+            qc_vec_foreach_range_ ## TEST ## _prop,   \
+            &qc_vec_info,                             \
+            &qc_size_t_info,                          \
+            &qc_size_t_info);
+
+QC_MKTEST_FOREACH_RANGE(content);
+QC_MKTEST_FOREACH_RANGE(iter);
+QC_MKTEST_FOREACH_RANGE(len);
+QC_MKTEST_FOREACH_RANGE(side_effects);
+
+QC_MKTEST_ALL(qc_vec_foreach_range_test_all,
+        qc_vec_foreach_range_content_test,
+        qc_vec_foreach_range_iter_test,
+        qc_vec_foreach_range_len_test,
+        qc_vec_foreach_range_side_effects_test,
+        );
