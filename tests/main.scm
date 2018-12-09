41c0f06e6e752240d1f94fa7582444f851dafa2a
siiky <siiky@users.noreply.github.com>
2018-12-09 12:51:01 +0000

    tests: Replace main.c with main.scm
    Replace the main C code with CHICKEN Scheme
    The compiler used to compile C code must be the same
      used to build CHICKEN

---
+++ tests/main.scm
@@ -0,0 +1,55 @@
+(import
+  chicken.base
+  chicken.foreign
+  chicken.format
+  chicken.process-context
+  scheme
+  srfi-1
+  )
+
+#>
+#include "map/qc.h"
+#include "vec/qc.h"
+<#
+
+(define map-test-all `(map . ,(foreign-lambda bool "qc_map_test_all")))
+(define vec-test-all `(vec . ,(foreign-lambda bool "qc_vec_test_all")))
+
+(define *TESTS*
+  `(,map-test-all ,vec-test-all))
+
+(define (usage cmd)
+  (print
+    "Usage:\n"
+    "\t" cmd "\n"
+    "\t\tTest all modules\n"
+    "\t" cmd " x MOD...\n"
+    "\t\tTest all modules but the ones specified\n"
+    "\t" cmd " o MOD...\n"
+    "\t\tTest only the specified modules\n")
+  1)
+
+(define (run-all tests)
+  (fold (lambda (e acc) (+ acc (if ((cdr e)) 0 1))) 0 tests))
+
+(define (run-only tests mods)
+  (run-all (filter (lambda (test) (member (symbol->string (car test)) mods)) tests)))
+
+(define (run-except tests mods)
+  (run-all (filter (lambda (test) (not (member (symbol->string (car test)) mods))) tests)))
+
+; TODO: return value
+(define (main args)
+  (let ((usg    (lambda (_) (usage (program-name))))
+        (all    (lambda (_) (run-all    *TESTS*)))
+        (only   (lambda (_) (run-only   *TESTS* (cdr args))))
+        (except (lambda (_) (run-except *TESTS* (cdr args)))))
+    (exit
+      (cond
+        ((= (length args) 1)       => usg)
+        ((null? args)              => all)
+        ((string=? "o" (car args)) => only)
+        ((string=? "x" (car args)) => except)
+        (else                      => usg)))))
+
+(main (command-line-arguments))
