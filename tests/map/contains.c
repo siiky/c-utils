4ee7d122066b42cbf3fc10a5690f819c5c615f62
siiky <siiky@users.noreply.github.com>
2018-12-09 23:42:06 +0000

    tests: map: Improve MAP_CONTAINS() tests
    Avoid changing the original map

--- tests/map/contains.c
+++ tests/map/contains.c
@@ -17,11 +17,14 @@ static enum theft_trial_res QC_MKID_PROP(res) (struct theft * t, void * arg1, vo
 {
     UNUSED(t);
 
-    struct map * map = arg1;
+    const struct map * _map = arg1;
     QC_ARG2VAR(2, int, key);
 
-    bool res1 = qc_map_contains(map, key);
-    bool res2 = map_contains(map, key);
+    bool res1 = qc_map_contains(_map, key);
+
+    struct map map = *_map;
+    bool res2 = map_contains(&map, key);
+
     bool ret = (res1 && res2) || (!res1 && !res2);
 
     return QC_BOOL2TRIAL(ret);
@@ -31,24 +34,35 @@ static enum theft_trial_res QC_MKID_PROP(res_not_in) (struct theft * t, void * a
 {
     UNUSED(t);
 
-    struct map * map = arg1;
+    const struct map * _map = arg1;
     QC_ARG2VAR(2, int, key);
-    key = qc_map_random_not_in(map, key);
+
+    key = qc_map_random_not_in(_map, key);
+
     QC_ARG2VAL(2, int) = key;
-    return QC_BOOL2TRIAL(!map_contains(map, key));
+
+    struct map map = *_map;
+    bool ret = !map_contains(&map, key);
+
+    return QC_BOOL2TRIAL(ret);
 }
 
 static enum theft_trial_res QC_MKID_PROP(res_in) (struct theft * t, void * arg1, void * arg2)
 {
-    struct map * map = arg1;
+    const struct map * _map = arg1;
     QC_ARG2VAR(2, int, key);
 
-    if (qc_map_cardinal(map) == 0)
+    if (qc_map_cardinal(_map) == 0)
         return THEFT_TRIAL_SKIP;
 
-    key = qc_map_random_in(t, map);
+    key = qc_map_random_in(t, _map);
+
     QC_ARG2VAL(2, int) = key;
-    return QC_BOOL2TRIAL(map_contains(map, key));
+
+    struct map map = *_map;
+    bool ret = map_contains(&map, key);
+
+    return QC_BOOL2TRIAL(ret);
 }
 
 QC_MKTEST_FUNC(res);
