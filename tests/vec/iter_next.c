322c50c3b73043ad51f86c443e4ff245c9bef704
siiky <siiky@users.noreply.github.com>
2018-12-15 01:22:37 +0000

    tests: vec: Remove SKIPS from VEC_ITER_NEXT()
    Remove unnecessary SKIPS from VEC_ITER_NEXT() tests

--- tests/vec/iter_next.c
+++ tests/vec/iter_next.c
@@ -18,9 +18,6 @@ static enum theft_trial_res QC_MKID_PROP(content) (struct theft * t, void * arg1
 
     struct vec * vec = arg1;
 
-    if (!vec->iterating)
-        return THEFT_TRIAL_SKIP;
-
     struct vec dup = {0};
     if (!qc_vec_dup_contents(vec, &dup))
         return THEFT_TRIAL_SKIP;
@@ -40,14 +37,11 @@ static enum theft_trial_res QC_MKID_PROP(idx_len) (struct theft * t, void * arg1
 
     struct vec * vec = arg1;
 
-    if (!vec->iterating)
-        return THEFT_TRIAL_SKIP;
-
     vec_iter_next(vec);
 
     return QC_BOOL2TRIAL((vec->length > 0) ?
-            vec->idx <  vec->length:
-            vec->idx == vec->length);
+            vec->idx < vec->length:
+            vec->idx == 0);
 }
 
 static enum theft_trial_res QC_MKID_PROP(idx) (struct theft * t, void * arg1)
@@ -56,16 +50,15 @@ static enum theft_trial_res QC_MKID_PROP(idx) (struct theft * t, void * arg1)
 
     struct vec * vec = arg1;
 
-    if (!vec->iterating)
-        return THEFT_TRIAL_SKIP;
-
     size_t pre_idx = vec->idx;
 
     vec_iter_next(vec);
 
     size_t pos_idx = vec->idx;
 
-    bool ret = (vec->reverse) ?
+    bool ret = (!vec->iterating) ?
+        pos_idx == pre_idx:
+        (vec->reverse) ?
         pos_idx <= pre_idx:
         pos_idx >= pre_idx;
 
